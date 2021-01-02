7021da51acd78518a0e6f807bb8b2b718b18b0af
Kevin Serra <kevindanielserra@gmail.com>
2021-01-02 11:36:55 -0500

    Changed formatting

--- src/commands/ICommand.ts
+++ src/commands/ICommand.ts
@@ -2,5 +2,5 @@ import tmi = require('tmi.js');
 
 export interface ICommand {
     name: string;
-    run(channel: string, tags: tmi.ChatUserstate, message: string, commandArgs: string[]):void;
+    run(channel: string, tags: tmi.ChatUserstate, message: string, commandArgs: string[]): void;
 }
