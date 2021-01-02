ade0a8ceb148d4f97b3b6c163e8aa1ed3e991b46
Kevin Serra <kevindanielserra@gmail.com>
2021-01-02 10:01:19 -0500

    Added description of invalid command

--- src/commands/invalid.ts
+++ src/commands/invalid.ts
@@ -3,6 +3,8 @@ import { client } from '../client';
 import { ICommand } from "./ICommand";
 import { Utils } from "../utils";
 
+// The invalid command is run when an invalid command is attempted.
+// Usage in chat: "!invalid" or attempt to use an undefined command.
 export class invalid implements ICommand {
     name: string = "invalid";
     run(channel: string, tags: tmi.ChatUserstate, message: string, commandArgs: string[]): void {
