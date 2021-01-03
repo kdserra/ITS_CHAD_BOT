c250a143e0546fe90dfa3236ab4b89630d515437
Kevin Serra <kevindanielserra@gmail.com>
2021-01-03 01:41:48 -0500

    Added chat message helpers

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,23 @@
+import tmi = require("tmi.js");
+import { client } from "./client";
 export { Utils }
 
 class Utils {
+    /*
+    const msg: string = ("@" + tags['display-name'] + " -> Output was cleared.");
+    Utils.PrintTimestamped("Sent Message: " + msg);
+    client.say(channel, msg);
+    */
+
+    public static SendChatMessage(channel:string, msg:string) {
+        client.say(channel, msg);
+        Utils.PrintTimestamped("Sent Message: " + msg);
+    }
+
+    public static SendChatMessageToPerson(channel:string, person: string, msg: string) {
+        Utils.SendChatMessage(channel, "@" + person + " -> " + msg);
+    }
+
     public static GetCurrentTime(): string {
         return Date().slice(16, 21);
     }
@@ -11,19 +28,19 @@ class Utils {
         return date
     }
     public static GetElapsedMinutes(startDate: Date, endDate: Date): number {
-        return Math.floor((Math.abs(startDate.getTime() - endDate.getTime())/1000)/60);
+        return Math.floor((Math.abs(startDate.getTime() - endDate.getTime()) / 1000) / 60);
     }
 
-    public static SetCharAt(str:string,index:number,chr:string) {
-        if(index > str.length-1) return str;
-        return str.substring(0,index) + chr + str.substring(index+1);
+    public static SetCharAt(str: string, index: number, chr: string) {
+        if (index > str.length - 1) return str;
+        return str.substring(0, index) + chr + str.substring(index + 1);
     }
 
-    public static PrintTimestamped(message:string){
+    public static PrintTimestamped(message: string) {
         console.log("[" + Utils.GetCurrentTime() + "] " + message);
     }
 
-    public static PrintLineTimestamped(){
+    public static PrintLineTimestamped() {
         Utils.PrintTimestamped("-----------------------------");
     }
 }
