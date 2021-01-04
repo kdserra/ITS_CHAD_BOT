7d451b0a7a1724909b47ff30cf104ddda773d2d8
Kevin Serra <kevindanielserra@gmail.com>
2021-01-04 06:46:11 -0500

    Removed TMI related Utilities
    Removed TMI related Utilities and moved them to tmi-utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,34 +1,6 @@
-import tmi = require("tmi.js");
-import { client } from "./client";
 export { Utils }
 
 class Utils {
-    public static IsMod(tags: tmi.ChatUserstate): boolean {
-        if (tags.mod === undefined || tags.mod === false) { return false; }
-        else { return true; }
-    }
-
-    public static IsStreamer(channel: string, tags: tmi.ChatUserstate): boolean {
-        return channel === tags.username;
-    }
-    public static IsStreamerOrMod(channel: string, tags: tmi.ChatUserstate): boolean {
-        return Utils.IsStreamer(channel, tags) || Utils.IsMod(tags);
-    }
-
-    public static GetDisplayNameFromTag(tags: tmi.ChatUserstate): string {
-        if (tags["display-name"] === undefined) { return ""; }
-        else { return tags["display-name"]; }
-    }
-
-    public static SendChatMessage(channel: string, msg: string): void {
-        client.say(channel, msg);
-        Utils.PrintTimestamped("Sent Message: " + msg);
-    }
-
-    public static SendChatMessageToPerson(channel: string, person: string, msg: string): void {
-        Utils.SendChatMessage(channel, "@" + person + " -> " + msg);
-    }
-
     public static GetCurrentTime(): string {
         return Date().slice(16, 21);
     }
