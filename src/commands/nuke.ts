a5b65c4fbda2ddbaa8955fbfdd0cd0802cade2af
Kevin Serra <kevindanielserra@gmail.com>
2021-01-04 03:51:03 -0500

    Updated to use helper functions

--- src/commands/nuke.ts
+++ src/commands/nuke.ts
@@ -10,14 +10,15 @@ import { LogEntry } from '../definitions/LogEntry';
 export class nuke implements ICommand {
     name: string = "nuke";
     run(channel: string, tags: tmi.ChatUserstate, message: string, commandArgs: string[]): void {
-        if (tags.mod || channel.slice(1, channel.length) === tags.username) {
+        const channel_name: string = channel.slice(1, channel.length);
+        if (Utils.IsStreamerOrMod(channel_name, tags)) {
             let minutes: number = parseInt(commandArgs[3]);
             if (commandArgs.length == 4 && !isNaN(minutes) && minutes > 0) {
                 if (minutes > 60) { minutes = minutes = 60; }
                 const matches: LogEntry[] = Log.FindDataInLogWithinTime(commandArgs[2], minutes);
                 for (let i = 0; i < matches.length; i++) {
-                    if (matches[i].tags.mod === false && channel.slice(1, channel.length) !== matches[i].tags.username) {
-                        client.ban(channel, matches[i].tags['display-name'], "Timed out by nuke command.");
+                    if (!Utils.IsStreamerOrMod(channel_name, matches[i].tags)) {
+                        client.ban(channel, matches[i].tags['display-name'], "Banned from the nuke command.");
                     }
                 }
             }
