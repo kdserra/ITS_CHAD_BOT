ce04d6dfa67ddf3e7185e8647f1d86d8aabbbbf2
Kevin Serra <kevindanielserra@gmail.com>
2021-01-04 03:50:51 -0500

    Updated to use helper functions, formatted code

--- src/commands/debug.ts
+++ src/commands/debug.ts
@@ -10,24 +10,25 @@ import { LogEntry } from '../definitions/LogEntry';
 export class debug implements ICommand {
     name: string = "debug";
     run(channel: string, tags: tmi.ChatUserstate, message: string, commandArgs: string[]): void {
-        if (tags.mod || channel.slice(1, channel.length) === tags.username) {
+        const channel_name = channel.slice(1, channel.length);
+        if (Utils.IsStreamerOrMod(channel_name, tags)) {
             if (commandArgs.length == 3) {
                 switch (commandArgs[2]) {
                     case "printlog":
                         Log.PrintLog();
-                        Utils.SendChatMessageToPerson(channel,tags['display-name'],"Log was printed, see output.");
+                        Utils.SendChatMessageToPerson(channel, tags['display-name'], "Log was printed, see output.");
                         break;
                     case "clear":
                         for (let i = 0; i < 20; i++) { Utils.PrintEmpty(); }
-                        Utils.SendChatMessageToPerson(channel,tags['display-name'],"Output was cleared.");
+                        Utils.SendChatMessageToPerson(channel, tags['display-name'], "Output was cleared.");
                         break;
                     default:
-                        Utils.SendChatMessageToPerson(channel,tags['display-name'],"The debug command you tried to use does not exist.");
+                        Utils.SendChatMessageToPerson(channel, tags['display-name'], "The debug command you tried to use does not exist.");
                         break;
                 }
             }
             else {
-                Utils.SendChatMessageToPerson(channel,tags['display-name'],"Incorrect usage: !debug <debug cmd>.");
+                Utils.SendChatMessageToPerson(channel, tags['display-name'], "Incorrect usage: !debug <debug cmd>.");
             }
         }
     }
