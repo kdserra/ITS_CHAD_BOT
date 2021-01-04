2e0855be883fd4753b8a785932f41bd20a9238cc
Kevin Serra <kevindanielserra@gmail.com>
2021-01-03 23:19:41 -0500

    Changed log output, removes old logs,  and change interval to 1 hour

--- src/log.ts
+++ src/log.ts
@@ -1,4 +1,6 @@
 import { log } from "console";
+import { start } from "repl";
+import { channels, client } from "./client";
 import { LogEntry } from "./definitions/LogEntry"
 import { Utils } from "./utils";
 export { Log }
@@ -8,10 +10,16 @@ class Log {
 
     public static AddToLog(logEntry: LogEntry): void {
         this.log.push(logEntry);
-        setInterval(function () { Log.RemoveFromLog(logEntry); }, 60000);
+        setTimeout(function () { Log.RemoveFromLog(logEntry); }, 3600000);
     }
 
     public static RemoveFromLog(logEntry: LogEntry): boolean {
+        Utils.PrintLineTimestamped();
+        Utils.PrintTimestamped("Removed log:");
+        Utils.PrintTimestamped("Username: " + logEntry.tags.username);
+        Utils.PrintTimestamped("Data: " + logEntry.data);
+        Utils.PrintTimestamped("Time Added: " + logEntry.timeAdded);
+        Utils.PrintLineTimestamped();
         const index = this.log.indexOf(logEntry);
         if (index > -1) {
             this.log.splice(index, 1);
@@ -30,10 +38,27 @@ class Log {
         return matches;
     }
 
-    public static PrintLog() {
-        if (this.log.length > 0) {
-            Utils.PrintLineTimestamped();
+    public static FindDataInLogWithinTime(data: string, minutes: number): LogEntry[] {
+        let matches: LogEntry[] = [];
+        for (let i = 0; i < this.log.length; i++) {
+
+            let startTime: Date = Utils.GetDateFromTime(this.log[i].timeAdded);
+            let endTime: Date = new Date();
+            endTime.setMinutes(endTime.getMinutes() - minutes);
+
+            if (Utils.GetElapsedMinutes(startTime, endTime) <= minutes) {
+                if (this.log[i].data.toLowerCase().includes(data)) {
+                    matches.push(this.log[i]);
+                }
+            }
         }
+        return matches;
+    }
+
+    public static PrintLog() {
+        Utils.PrintLineTimestamped();
+        Utils.PrintTimestamped("Chat Log")
+        Utils.PrintLineTimestamped();
         for (let i = 0; i < this.log.length; i++) {
             Utils.PrintTimestamped("Entry Number: " + i);
             Utils.PrintTimestamped("Username: " + this.log[i].tags.username);
@@ -41,5 +66,7 @@ class Log {
             Utils.PrintTimestamped("Time Added: " + this.log[i].timeAdded);
             Utils.PrintLineTimestamped();
         }
+        Utils.PrintEmpty();
+        Utils.PrintLineTimestamped();
     }
 }
